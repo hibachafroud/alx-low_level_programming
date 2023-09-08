802a1275f6f5ed7b69d3a797fc0301b6bda94487
hibachafroud <hiba.chafroud4@gmail.com>
2023-09-07 17:54:54 -0700

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print this message "and that piece of art is useful"
+ *                 - Dora Korpar, 2015-10-19"
+ *
+ * Return:  1 Sucess .
+ */
+
+int main(void)
+{
+	putchar("and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
