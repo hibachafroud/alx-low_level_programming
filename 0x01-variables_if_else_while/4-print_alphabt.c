75c6454a66e8f698bd437e1ead952d2483ccbf4c
hibachafroud <hiba.chafroud4@gmail.com>
2023-09-08 19:14:35 -0700

    print all alphabet except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		if (alphabet != 'e' && alphabet != 'q')
 		{
-		    putchar(alphabet);
+		putchar(alphabet);
 		}
 	}
 	putchar('\n');
