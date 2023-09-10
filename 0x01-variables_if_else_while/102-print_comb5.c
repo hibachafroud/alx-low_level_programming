9f53bdf5f54ad217fe8039a37f200499308db501
hibachafroud <hiba.chafroud4@gmail.com>
2023-09-09 17:00:39 -0700

    combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <ctype.h>
 
 /**
- * main - Print  all possible different combinations of two digits from 0 to 9
+ * main - Print  all possible different combinations of two two-digits from
+ *      Note: ascii code only supported from 0 to 9
  * Return:  0 Always .
  */
 
@@ -10,17 +10,17 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 48; digit1 <= 146; digit1++)
+	for (digit1 = 0; digit1 <= 98; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 146; digit2++)
+		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
 		{
-		    putchar(digit1);
-		    putchar(digit1);
-		    putchar(' ');
-			putchar(digit2);
-			putchar(digit2);
+			putchar((digit1 / 10) + '0');
+			putchar((digit1 % 10) + '0');
+			putchar(' ');
+			putchar((digit2 / 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-			if (digit1 < 145 || digit2 < 146)
+			if (digit1 < 98 && digit2 < 99)
 			{
 			putchar(',');
 			putchar(' ');
