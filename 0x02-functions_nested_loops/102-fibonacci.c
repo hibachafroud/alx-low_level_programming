c5782481b2b93eae8b249ff73757134b704caa6e
hibachafroud <hiba.chafroud4@gmail.com>
2023-09-12 19:18:43 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers,
+ *                  starting with 1 and 2
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int count = 0;
+	int next;
+	int fib1 = 0, fib2 = 1;
+
+	while (i < 1024)
+	{
+		next = fib1 + fib2;
+	    printf("%d, ", next);
+	    fib1 = fib2;
+	    fib2 = next;
+	    count++;
+	}
+	printf("\n");
+	return (0);
+}
