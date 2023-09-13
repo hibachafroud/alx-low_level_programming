27ad7ca59283737d8dfc96be9df1f19c0b4fbe18
hibachafroud <hiba.chafroud4@gmail.com>
2023-09-12 17:56:51 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,35 +15,29 @@ void print_times_table(int n)
 	{
 		for (digit1 = 0; digit1 <= n; digit1++)
 		{
-			for (digit2 = 0; digit2 <= n; digit2++)
+		    _putchar('0');
+
+			for (digit2 = 0; digit2 <= n; digit1++)
 			{
+			    _putchar(',');
+				_putchar(' ');
 				mult = digit1 * digit2;
 
-				if (digit1 == 0)
+				if (mult < 10 && digit1 != 0)
 				{
-					_putchar(mult + '0');
-				}
-				else if (mult < 10 && digit2 != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(mult + '0');
 				}
 				else if (mult >= 10 && mult < 100)
 				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 					_putchar((mult / 10) + '0');
 					_putchar((mult % 10) + '0');
 				}
 				else if (mult >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((mult / 100 )+ '0');
+					_putchar((mult / 100) + '0');
 					_putchar((mult / 10) % 10) + '0');
 					_putchar((mult % 10) + '0');
 				}
