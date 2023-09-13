e59aa245064afff3457f55dc4b67c53b768f4c5e
hibachafroud <hiba.chafroud4@gmail.com>
2023-09-12 18:26:28 -0700

    print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times_table - prints the n times table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  *              If n is greater than 15 or less than 0
  *              the function should not print anything
  * @n :the number of times table
@@ -14,35 +14,31 @@ void print_times_table(int n)
 	{
 	for (digit1 = 0; digit1 <= n; digit1++)
 	{
-		    _putchar('0');
+		_putchar('0');
 
-			for (digit2 = 1; digit2 <= n; digit1++)
+		for (mult = 1; mult <= n; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			mult = digit1 * digit2;
+
+			if (mult <  10 && mult < 100)
 			{
-				mult = digit1 * digit2;
-
-				_putchar(',');
-				_putchar(' ');
-
-				if (mult < 10 && digit1 != 0)
-				{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(mult + '0');
-				}
-				else if (mult >= 10 && mult < 100)
-				{
-				_putchar(' ');
-				_putchar((mult / 10) + '0');
-				_putchar((mult % 10) + '0');
-				}
-				else if (mult >= 100)
-				{
-				_putchar((mult / 100) + '0');
-				_putchar(((mult / 10) % 10) + '0');
-				_putchar((mult % 10) + '0');
-				}
+			_putchar(' ');
 			}
-			_putchar('\n');
+			else if (mult < 100 && mult >= 10)
+			{
+			_putchar((mult / 10) + '0');
+			}
+			else if (mult >= 100)
+			{
+			_putchar((mult / 100) + '0');
+			_putchar(((mult / 10)) % 10 + '0');
+			}
+			_putchar((mult % 10) + '0');
+			}
+		_putchar('\n');
 		}
 	}
 }
