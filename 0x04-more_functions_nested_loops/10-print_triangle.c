17a8e7ac283cf50a031bc8ff91cd9eef7ac0116c
hibachafroud <hiba.chafroud4@gmail.com>
2023-09-14 16:48:53 -0700

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle on the terminal
+ * @size:size is the number of times the character # should be printed
+ *
+ * if size is 0 or less, the function should only print a '\n'
+ * the diagonal should end with a '\n'
+ *
+ * Return: void ;
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				putchar(32);
+			}
+
+			for (k = 1; k <= i; k++)
+			{
+				putchar(35);
+			}
+
+			putchar('\n');
+		}
+	}
+	else
+	{
+	   putchar('\n');
+	}
+}
