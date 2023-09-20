bfb887d9560eb3336de0b86b63611d33f4f02b40
hibachafroud <hiba.chafroud4@gmail.com>
2023-09-20 14:45:20 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int tmp, i;
+	int i, tmp;
 
 	for (i = 0; i < (n / 2); i++)
 	{
 		tmp = a[i];
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = tmp;
-		n--;
 	}
 }
