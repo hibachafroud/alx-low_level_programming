87fcc7b6113abad5eae0c0bb34e286417b2b6df5
hibachafroud <hiba.chafroud4@gmail.com>
2023-09-21 14:55:04 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,20 @@
 
 void print_number(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
-		n = -n;
+		n1 = -n;
 		_putchar('-');
 	}
-	if (n / 10 != 0)
+	else
 	{
-		print_number(n / 10);
+		n1 = n;
 	}
-	_putchar((n % 10) + '0');
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
